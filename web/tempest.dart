c2cade94896e353da4e5ecfb85a49ec4f7795a6f
Vaino Jarvela <vaino@jarve.la>
2014-07-05 08:59:08 +0300

    Removed old copyright
    Doesn't actually refer to Solar3d anymore.

--- web/tempest.dart
+++ web/tempest.dart
@@ -1,7 +1,3 @@
-// Heavily borrows structure from Solar3D dart-sample
-// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
 // Copyright (c) 2014, Väinö Järvelä.
 
 library tempest;
