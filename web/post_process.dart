eda3181ba58eba27ad82e1ed23686efafda1fe80
Vaino Jarvela <vaino@jarve.la>
2014-07-06 11:22:40 +0300

    Bloom adjustments

--- web/post_process.dart
+++ web/post_process.dart
@@ -196,10 +196,10 @@ class GaussianHorizontalPass extends PostProcessPass {
     gl.bindTexture(WebGL.TEXTURE_2D, _inputTex);
     gl.uniform1i(_uSampler0, 0);
 
-    gl.uniform1f(_uSize, 1.0 / _width);
+    gl.uniform1f(_uSize, 2.0 / _width);
     gl.uniform1i(_uBlurAmount, 10);
-    gl.uniform1f(_uBlurScale, 3.0);
-    gl.uniform1f(_uBlurStrength, 0.8);
+    gl.uniform1f(_uBlurScale, 2.0);
+    gl.uniform1f(_uBlurStrength, 0.9);
   }
 
   @override
@@ -246,13 +246,14 @@ class BlendPass extends PostProcessPass {
       vec4 dst = texture2D(uSampler0, vTexCoord);
       vec4 src = texture2D(uSampler1, vTexCoord);
 
-      //gl_FragColor = clamp((src + dst) - (src * dst), 0.0, 1.0);
-      //gl_FragColor.w = 1.0;
-      src = (src * 0.5) + 0.5;
-      gl_FragColor.xyz = vec3((src.x <= 0.5) ? (dst.x - (1.0 - 2.0 * src.x) * dst.x * (1.0 - dst.x)) : (((src.x > 0.5) && (dst.x <= 0.25)) ? (dst.x + (2.0 * src.x - 1.0) * (4.0 * dst.x * (4.0 * dst.x + 1.0) * (dst.x - 1.0) + 7.0 * dst.x)) : (dst.x + (2.0 * src.x - 1.0) * (sqrt(dst.x) - dst.x))),
-        (src.y <= 0.5) ? (dst.y - (1.0 - 2.0 * src.y) * dst.y * (1.0 - dst.y)) : (((src.y > 0.5) && (dst.y <= 0.25)) ? (dst.y + (2.0 * src.y - 1.0) * (4.0 * dst.y * (4.0 * dst.y + 1.0) * (dst.y - 1.0) + 7.0 * dst.y)) : (dst.y + (2.0 * src.y - 1.0) * (sqrt(dst.y) - dst.y))),
-        (src.z <= 0.5) ? (dst.z - (1.0 - 2.0 * src.z) * dst.z * (1.0 - dst.z)) : (((src.z > 0.5) && (dst.z <= 0.25)) ? (dst.z + (2.0 * src.z - 1.0) * (4.0 * dst.z * (4.0 * dst.z + 1.0) * (dst.z - 1.0) + 7.0 * dst.z)) : (dst.z + (2.0 * src.z - 1.0) * (sqrt(dst.z) - dst.z))));
+      src = src * 0.3;
+      gl_FragColor = clamp((src + dst) - (src * dst), 0.0, 1.0);
       gl_FragColor.w = 1.0;
+      //src = (src * 0.5) + 0.5;
+      //gl_FragColor.xyz = vec3((src.x <= 0.5) ? (dst.x - (1.0 - 2.0 * src.x) * dst.x * (1.0 - dst.x)) : (((src.x > 0.5) && (dst.x <= 0.25)) ? (dst.x + (2.0 * src.x - 1.0) * (4.0 * dst.x * (4.0 * dst.x + 1.0) * (dst.x - 1.0) + 7.0 * dst.x)) : (dst.x + (2.0 * src.x - 1.0) * (sqrt(dst.x) - dst.x))),
+      //  (src.y <= 0.5) ? (dst.y - (1.0 - 2.0 * src.y) * dst.y * (1.0 - dst.y)) : (((src.y > 0.5) && (dst.y <= 0.25)) ? (dst.y + (2.0 * src.y - 1.0) * (4.0 * dst.y * (4.0 * dst.y + 1.0) * (dst.y - 1.0) + 7.0 * dst.y)) : (dst.y + (2.0 * src.y - 1.0) * (sqrt(dst.y) - dst.y))),
+      //  (src.z <= 0.5) ? (dst.z - (1.0 - 2.0 * src.z) * dst.z * (1.0 - dst.z)) : (((src.z > 0.5) && (dst.z <= 0.25)) ? (dst.z + (2.0 * src.z - 1.0) * (4.0 * dst.z * (4.0 * dst.z + 1.0) * (dst.z - 1.0) + 7.0 * dst.z)) : (dst.z + (2.0 * src.z - 1.0) * (sqrt(dst.z) - dst.z))));
+      //gl_FragColor.w = 1.0;
     }
     ''';
 
